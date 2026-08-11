b9d36826058b40b4439dd88564185f940a9a95f2
boddepallihemavarun <boddepallihemavarun@gmail.com>
2026-08-11 13:45:16 +0530

    Updates

---
+++ chunks/Co1NCh8k.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-zkM91aFI.js";const a=[{title:"Final Skin Care App",slug:"skincare",thumbnail:e,description:"KI-gestützte Hautpflege-Empfehlungen"}];export{a as default};
---
+++ chunks/qdvpiNt6.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-zkM91aFI.js";const n=[{title:"Final Skin Care App",slug:"skincare",thumbnail:e,description:"AI-powered skincare recommendations"}];export{n as default};
